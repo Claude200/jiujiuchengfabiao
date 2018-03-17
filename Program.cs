abf59fe6641143804240c947b95ba40d46b89782
Claude200 <762420244@qq.com>
2018-03-17 12:00:51 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write(string.Format("{0}*{1}={2}", i, j, i * j));
+                    //Console.Write($"{ i }*{ j }={ i*j }");    C#6.0以上的版本支持
+                Console.WriteLine();
+            }
+         }
+    }
+}
